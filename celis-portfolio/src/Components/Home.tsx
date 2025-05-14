6878b3aff973aa96a200dae892243dcca201928e
Jomari Quiñonez Celis <jomaricelis@gmail.com>
2025-05-14 18:56:53 +0800

    Add profile image and update Home component layout; refactor font-face definitions in CSS

--- celis-portfolio/src/Components/Home.tsx
+++ celis-portfolio/src/Components/Home.tsx
@@ -1,11 +1,16 @@
 import "../index.css";
-
+import profileImage from "../assets/Images/Placeholder1.png";
 export default function Home(){
     return(
-        <div className="text-stone-900 font-Garamond text-8xl font-semibold">
-            Jomari Celis
+        <div className=" border h-screen items-center font-Garamond flex flex-col bg-white h-full">
+            <div className="pt-16">
+                <p className="text-stone-900 text-8xl font-semibold">Jomari Celis</p>
+            </div>
+
+            <div className="bottom-0 absolute">
+            <img className="h-auto w-xl" src={profileImage} alt="Placeholder " />
+            </div>
         </div>
 
-        
     )
 }
