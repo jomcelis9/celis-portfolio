ae322c6c7ac01acef4d35dc8563b04f2c6984528
Jomari Quiñonez Celis <jomaricelis@gmail.com>
2025-05-16 14:00:44 +0800

    Added Mesh Circle

--- celis-portfolio/src/Components/Home.tsx
+++ celis-portfolio/src/Components/Home.tsx
@@ -1,5 +1,6 @@
 import "../index.css";
 import profileImage from "../assets/Images/Placeholder1.png";
+import meshCircle from "../assets/Images/Mesh Circle.svg";
 export default function Home(){
     return(
     <div className="absolute inset-0 -z-10 h-full w-full bg-slate-900 bg-[linear-gradient(to_right,rgba(240,240,240,0.2)_0px,transparent_1px),linear-gradient(to_bottom,rgba(240,240,240,0.2)_0px,transparent_1px)] bg-[size:18rem_12rem] bg-[position:center] flex-col items-center">
@@ -15,10 +16,8 @@ export default function Home(){
                     
                 </div>
 
-                <div className="border m-5 p-5">
-
-                Right border
-
+                <div className="flex flex-col items-center border m-5 p-5 ">
+                <img className="w-100" src={meshCircle}/>                
                 </div>
             </div>
 
