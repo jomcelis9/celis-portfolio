96c598633f1268093d07c1ac042ee2b7d45834c6
Jomari Quiñonez Celis <jomaricelis@gmail.com>
2025-05-17 14:27:26 +0800

    Refactor Home component layout; improve structure and styling for mesh circles

--- celis-portfolio/src/Components/Home.tsx
+++ celis-portfolio/src/Components/Home.tsx
@@ -7,35 +7,38 @@ import meshCircle2 from "../assets/Images/Mesh Circle2.svg";
 export default function Home(){
     return(
         
-    <div className="font-Satoshi absolute inset-0 -z-10 h-screen w-full bg-slate-900 bg-[linear-gradient(to_right,rgba(240,240,240,0.2)_0px,transparent_1px),linear-gradient(to_bottom,rgba(240,240,240,0.2)_0px,transparent_1px)] bg-[size:18rem_12rem] bg-[position:center] flex-col items-center">            
-            <div className=" mt-5 pt-8 font-Clash mb-2">
+    <div className="min-h-screen w-full bg-slate-900 bg-[linear-gradient(to_right,rgba(240,240,240,0.2)_0px,transparent_1px),linear-gradient(to_bottom,rgba(240,240,240,0.2)_0px,transparent_1px)] bg-[size:18rem_12rem] bg-[position:center] items-center">
+        <div className=" pt-8 font-Clash mb-2">
                 <p className= "text-center text-slate-50 lg:text-7xl sm:text-5xl font-regular ">Jomari Celis</p>
             </div>    
 
-            <div>
+            <div className="mx-5">
 
                 <div className="font-Satoshi">
                     <p className= "text-center text-slate-50 text-xl font-regular ">pick a jom!</p>
                 </div>
                 <div id="meshCircles" className="grid lg:grid-cols-2 md:grid-cols-1  items-center text-white mt-5 pl">
-                <div  className="flex flex-col items-center border m-0 p-0">
-                    <div>
-                        <img className="w-180 w-50" src={meshCircle2}/>  
-                    </div>
-                    <div className="font-Clash text-8xl bottom-10 m-5 absolute z-10">
-                        <p>Multimedia Portfolio</p>
-                    </div>        
-                </div>
+                        <div className="flex flex-col items-center m-0 p-0">
+                            <div>
+                                <img className="lg:w-180 md:w-100" src={meshCircle2}/>  
+                            </div>
 
-                <div className="flex flex-col items-center border m-0 p-0">
-                    <div>
-                        <img className="w-180 w-50" src={meshCircle}/>  
-                    </div>
-                    <div className="font-Clash text-8xl bottom-10 m-5 absolute z-10">
-                        <p>Programming Portfolio</p>
-                    </div>        
+                            <div className="absolute text-center font-Clash lg:text-7xl md:text-6xl sm:text-5xl lg:bottom-20 m-5 z-10">
+                                <p>Multimedia <br /> Portfolio</p>
+                            </div> 
+                        </div>
+
+
+                        <div className="flex flex-col items-center m-0 p-0">
+                            <div>
+                                <img className="lg:w-180 md:w-100" src={meshCircle}/>  
+                            </div>
+
+                            <div className="absolute text-center font-Clash lg:text-7xl md:text-6xl  sm:text-xl lg:bottom-20 m-5 z-10">
+                                <p>Programming <br /> Portfolio</p>
+                            </div> 
+                        </div>
                 </div>
-            </div>
 
             </div>
 
