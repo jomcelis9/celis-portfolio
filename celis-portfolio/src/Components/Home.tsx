215dc76f5d1f7fa308cbd129382fb4522e1412ac
Jomari Quiñonez Celis <jomaricelis@gmail.com>
2025-05-17 16:45:39 +0800

    First Page Animation

--- celis-portfolio/src/Components/Home.tsx
+++ celis-portfolio/src/Components/Home.tsx
@@ -2,6 +2,7 @@ import "../index.css";
 import profileImage from "../assets/Images/Placeholder1.png";
 import meshCircle from "../assets/Images/Mesh Circle.svg";
 import meshCircle2 from "../assets/Images/Mesh Circle2.svg";
+import {motion, vw} from "motion/react"
 
 
 export default function Home(){
@@ -12,31 +13,73 @@ export default function Home(){
                 <p className= "text-center text-slate-50 lg:text-7xl sm:text-5xl font-regular ">Jomari Celis</p>
             </div>    
 
-            <div className="mx-5">
+            <div className="mx-5"> 
 
-                <div className="font-Satoshi">
-                    <p className= "text-center text-slate-50 text-xl font-regular ">pick a jom!</p>
-                </div>
-                <div id="meshCircles" className="grid lg:grid-cols-2 md:grid-cols-1  items-center text-white mt-5 pl">
-                        <div className="flex flex-col items-center m-0 p-0">
-                            <div>
-                                <img className="lg:w-180 md:w-100" src={meshCircle2}/>  
-                            </div>
+            <div className="font-Satoshi">
+                <p className="text-center text-slate-50 text-xl font-regular">pick a jom!</p>
+            </div>
+
+            <div id="meshCircles" className="flex justify-center items-center gap-x-10 text-white mt-5">
+                    {/* Orange Circle */}
+                    <div className="flex flex-col items-center">
+                        <div>
+                            <motion.img 
+                                className="lg:w-180 md:w-100" 
+                                src={meshCircle2} 
+                                initial={{ 
+                                    rotate: 0,
+                                    x: "58%"
+                                }} 
+                                animate={{ 
+                                    rotate: 360,
+                                    x: "0"
+                                }} 
+                                transition={{ delay:2, duration: 2, ease: "linear" }}
+                            />  
+                        </div>
 
                             <div className="absolute text-center font-Clash lg:text-7xl md:text-6xl sm:text-5xl lg:bottom-20 m-5 z-10">
                                 <p>Multimedia <br /> Portfolio</p>
                             </div> 
                         </div>
 
-
-                        <div className="flex flex-col items-center m-0 p-0">
+                        {/* Blue Circle  */}
+                        <div className="flex flex-col items-center">
                             <div>
-                                <img className="lg:w-180 md:w-100" src={meshCircle}/>  
+                                <motion.img 
+                                    className="lg:w-180 md:w-100" 
+                                    src={meshCircle} 
+                                    initial={{ 
+                                        rotate: 0,
+                                        x: "-56%"
+                                    }} 
+                                    animate={{ 
+                                        rotate: 360,
+                                        x: "0"
+                                    }} 
+                                    transition={{ delay:2, duration: 2, ease: "linear" }}
+                                />  
                             </div>
 
                             <div className="absolute text-center font-Clash lg:text-7xl md:text-6xl  sm:text-xl lg:bottom-20 m-5 z-10">
                                 <p>Programming <br /> Portfolio</p>
                             </div> 
+                        </div>
+
+                        <div className="absolute left-1/2 transform -translate-x-1/2 col-span-2 w-178.5 z-20">
+                            <svg className="w-165" viewBox="0 0 984 982" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <motion.circle
+                                    initial={{pathLength:0}} 
+                                    whileInView={{pathLength:1}}
+                                    transition={{
+                                        duration: 2,
+                                        ease: 'easeInOut'
+                                    }}
+                                    cx="492" 
+                                    cy="491" 
+                                    r="491" 
+                                    stroke="#F8FAFC"/>
+                            </svg>
                         </div>
                 </div>
 
