8601b4d0641f208389dad7a91881238d41940c8e
Jomari Quiñonez Celis <jomaricelis@gmail.com>
2025-05-23 01:26:15 +0800

    Refactor Home component; update animation parameters and improve element positioning

--- celis-portfolio/src/Components/Home.tsx
+++ celis-portfolio/src/Components/Home.tsx
@@ -22,7 +22,7 @@ export default function Home(){
 
      const entranceAnimationBlue = async () => {
         
-      await animate("#blueCircle", { opacity: 1 }, { duration: 3, ease: easeInOut });
+      await animate("#blueCircle", { opacity: 1}, { duration: 3, ease: easeInOut });
       await animate("#blueCircle", { x: 0 }, {duration: 3, ease: easeInOut})
 
      };
@@ -41,7 +41,7 @@ export default function Home(){
      const handleClick = () =>{
         setIsClick(true)
 
-        animate("#blueCircle", {x: "-57%", scale: 1.3}, {duration: 3})
+        animate("#blueCircle", {x: "-58%", filter: "blur(25px)" ,scale: 1.5}, {duration: 3})
         
         animate("#orangeCircleClass", { opacity: 0 }, {duration: 3})
 
@@ -57,22 +57,16 @@ export default function Home(){
     <div ref={scope}  className="bg-slate-900 bg-cover fixed">
         <motion.div initial={{opacity:"0%"}} transition={{duration:1, ease:easeIn}} animate={{opacity:"100%"}} className="min-h-screen w-full bg-slate-900 bg-[linear-gradient(to_right,rgba(240,240,240,0.2)_0px,transparent_1px),linear-gradient(to_bottom,rgba(240,240,240,0.2)_0px,transparent_1px)] bg-[size:18rem_12rem] bg-[position:center] items-center">
             <div className=" pt-8 font-Clash mb-2">
-                    <p id="myName" className= "text-center text-slate-50 lg:text-7xl sm:text-5xl font-regular ">Jomari Celis</p>
             </div>    
                 <div className="mx-5"> 
-
-                    <div className="font-Satoshi">
-                        <p className="text-center text-slate-50 text-xl font-regular">pick a jom!</p>
-                    </div>
-
-                        <div id="meshCircles" className="flex flex-col md:flex-row justify-center items-center gap-x-10 text-white mt-5">
+                        <div id="meshCircles" className="border flex flex-col md:flex-row justify-center items-center gap-x-10 text-white mt-5">
                                 {/* Orange Circle */}
                                 <div id="orangeCircleClass" className="flex flex-col items-center relative">
                                     <div>
                                         <motion.img
                                             
                                             id="orangeCircle" 
-                                            initial= {{opacity:0, x:"50%"}}
+                                            initial= {{opacity:0, x:"50vh"}}
                                             className="lg:w-180 md:w-100" 
                                             src={meshCircle2} 
                                         />  
@@ -92,8 +86,8 @@ export default function Home(){
                                             <motion.img
                                                 
                                                 id="blueCircle"
-                                                initial= {{ opacity: 0, x: "-50%" }}
-                                                className="lg:w-180 md:w-100" 
+                                                initial= {{ opacity: 0, x: "-50vh" }}
+                                                className="lg:w-180 md:w-10" 
                                                 src={meshCircle} 
                                                 onClick={handleClick}
 
